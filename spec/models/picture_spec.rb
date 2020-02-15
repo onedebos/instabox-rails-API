d3f3f589ff3d33fa87a75b52b75ffc15e5164c8e
Adebola <onedebos@gmail.com>
2020-02-15 18:54:45 +0100

    models done with tests

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Todo, type: :model do
-  it { should have_many(:items).dependent(:destroy) }
+RSpec.describe Picture, type: :model do
   it { should validate_presence_of(:img_link) }
   it { should validate_presence_of(:created_by) }
 end
