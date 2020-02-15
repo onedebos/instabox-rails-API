25dad4aea6fddec747a4806fe79b9e695121ff3e
Adebola <onedebos@gmail.com>
2020-02-15 19:02:37 +0100

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pictures/index'
+  get 'pictures/create'
+  get 'pictures/destroy'
+  get 'pictures/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
